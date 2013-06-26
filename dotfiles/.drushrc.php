a322d5174444172f7fe79cfd53ae584b6b50b574
gitUserName <gitUserEmail>
2013-06-25 23:19:22 -0400

    add drushrc

---
+++ dotfiles/.drushrc.php
@@ -0,0 +1,12 @@
+<?php
+
+$options['vagrant'] = array (
+  'username' => 'andrew',
+  'uid' => '501',
+  'gid' => '1001',
+  'git_name' => 'Andrew Oke',
+  'git_email' => 'gitUserEmail',
+  'home' => '/Users/andrew',
+  'project_root' => '/Users/andrew/vagrant/projects',
+  'dotfiles.profile' => '/Users/andrew/.profile',
+);
